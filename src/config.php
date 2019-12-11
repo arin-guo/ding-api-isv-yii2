15a365f54fd1fb84851bdbdb6289539fd6264b2e
qiang.guo <hzzt1234>
2019-12-11 17:21:48 +0800

    gitignore

--- src/config.php
+++ src/config.php
@@ -2,7 +2,7 @@
 define('DIR_ROOT', dirname(__FILE__).'/');
 define("OAPI_HOST", "https://oapi.dingtalk.com");
 //Suite
-define("CREATE_SUITE_KEY", "suite4xxxxxxxxxxxxxxx");
+define("CREATE_SUITE_KEY", "suite4xxxxxxxxxxxxxx");
 define("SUITE_KEY", "");
 define("SUITE_SECRET", "");
 define("TOKEN", "");
